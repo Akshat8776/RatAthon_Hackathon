1abe25805640956b57c3da3d3eb3dec3dcd9f1e1
Akshat Sharma <93507527+Akshat8776@users.noreply.github.com>
2023-11-20 23:19:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,41 +9,17 @@ data = {
         'Blog image URL':[],
         'Blog likes count':[]
     }
-print(1)
 df = pd.DataFrame(data)
-
 for mn in range(1,46):
-    # To fetch the data
     url=f'https://rategain.com/blog/page/{mn}/'
-    # print(2)
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-    print(3)
     web_byte = urlopen(req).read()
-    print(f'Hi{mn}')
     webpage = web_byte.decode('utf-8')
     soup=BeautifulSoup(web_byte,'html.parser')
-    # to save the data
-    # with open('readme.txt', 'w') as f:
-    #     f.write(str(web_byte))
-
-    
-    # file1 = open("readme.txt","r+")
-
-    # soup=BeautifulSoup(file1.read(),'html.parser')
-
     table = soup.findAll('div',attrs = {'class':"content"})
     for t in table:
-        # print(t)//success
-        # print("\n")
         s = str(t.find('a').text)
-        # print(s.replace("\\xe2\\x80\\x99","'")) # name fetch success
-        # print(t.a['href'])#link fetch successful
-        # print(t.span.text) # date fetch success
-        # like count success
         c= t.findAll('span')
-        # print(c)
-        # print(c[3].text)
-        # print()
         new_row = {
             'Blog title': s.replace("\\xe2\\x80\\x99","'"),
             'Blog date':t.span.text,
@@ -52,7 +28,5 @@ for mn in range(1,46):
         }
         df.loc[len(df)] = new_row
     time.sleep(2)
-    
 df.to_csv('RateGainHackathon.csv')
 print(df)
-    # print(web_byte)
